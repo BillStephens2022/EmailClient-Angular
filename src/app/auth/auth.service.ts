07d9bbe82f12eecff56f658bacd195402d90050c
BillStephens2022 <two4onebill@yahoo.com>
2023-09-16 13:38:09 -0400

    added submit functionality on signup form with error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,21 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface usernameAvailableResponse {
-  available: boolean
+interface UsernameAvailableResponse {
+  available: boolean;
+}
+
+interface SignupCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface SignupResponse {
+  username: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com';
 
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
-    return this.http
-      .post<usernameAvailableResponse>('https://api.angular-email.com/auth/username', {
-        username
-      });
+    return this.http.post<UsernameAvailableResponse>(
+      `${this.rootUrl}/auth/username`,
+      {
+        username,
+      }
+    );
+  }
+
+  signup(credentials: SignupCredentials) {
+    return this.http.post<SignupResponse>(
+      `${this.rootUrl}/auth/signup`,
+      credentials
+    );
   }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatchPassword } from '../validators/match-password';
 import { UniqueUsername } from '../validators/unique-username';
+import { AuthService } from '../auth.service';
+
+
+interface FormData {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
 
 @Component({
   selector: 'app-signup',
@@ -9,28 +17,54 @@ import { UniqueUsername } from '../validators/unique-username';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
-  authForm = new FormGroup(
-    {
-      username: new FormControl('', [
+  authForm = new FormGroup({
+    username: new FormControl(
+      '',
+      [
         Validators.required,
         Validators.minLength(3),
         Validators.maxLength(20),
         Validators.pattern(/^[a-z0-9]+$/),
-      ], [this.uniqueUsername.validate]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(20),
-      ]),
-      passwordConfirmation: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(20),
-      ]),
-    },
-  );
+      ],
+      [this.uniqueUsername.validate]
+    ),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+  });
 
-  constructor(private matchPassword: MatchPassword, private uniqueUsername: UniqueUsername) {
+  constructor(
+    private matchPassword: MatchPassword,
+    private uniqueUsername: UniqueUsername,
+    private authService: AuthService
+  ) {
     this.authForm.setValidators(this.matchPassword.validate);
+  }
+
+  onSubmit() {
+    if (this.authForm.invalid) {
+      return;
+    }
+    const formData = this.authForm.value as FormData;
+
+    this.authService.signup(formData).subscribe({
+      next: (response) => {
+        // Navigate to some other route
+      },
+      error: (err) => {
+        if (!err.status) {
+          this.authForm.setErrors({ noConnection: true });
+        } else {
+          this.authForm.setErrors({ unknownError: true})
+        }
+      }
+    });
   }
 }
