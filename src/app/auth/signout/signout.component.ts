1e262946c11e2fc186ee59cbd0ada3da229e5925
BillStephens2022 <two4onebill@yahoo.com>
2023-09-17 16:41:08 -0400

    implemented signout upon navigating to signout route

---
+++ src/app/auth/signout/signout.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-signout',
+  templateUrl: './signout.component.html',
+  styleUrls: ['./signout.component.css'],
+})
+export class SignoutComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.authService.signout().subscribe(() => {
+      // Navigate user back to the signin page
+      this.router.navigateByUrl('/');
+    });
+  }
+}
