fd99401ca95448b7db4f6cd9ba4371e41df4d0eb
BillStephens2022 <two4onebill@yahoo.com>
2023-09-21 13:46:32 -0400

    added functionality to send an email

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
+import { EmailService } from '../email.service';
 import { Email } from '../email';
 
 @Component({
@@ -11,7 +12,7 @@ export class EmailCreateComponent {
   showModal = false;
   email: Email;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private emailService: EmailService) {
     this.email = {
       id: '',
       to: '',
@@ -20,5 +21,11 @@ export class EmailCreateComponent {
       text: '',
       from: `${authService.username}@angular-email.com`
     }
+  }
+
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
   }
 }
