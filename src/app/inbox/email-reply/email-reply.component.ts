eadce00c6e84a34df4ddbf31cc3d7074f2c5bacd
BillStephens2022 <two4onebill@yahoo.com>
2023-09-21 14:08:38 -0400

    updated EmailReply component

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-reply',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./email-reply.component.css']
 })
 export class EmailReplyComponent {
+  showModal = false;
+  @Input() email: Email;
 
+  constructor() {
+    this.email = {
+      id: '',
+      to: '',
+      subject: '',
+      html: '',
+      text: '',
+      from: ''
+    };
+  }
+  ngOnInit() {
+    const text = this.email.text.replace(/\n/gi, '\n> ');
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n----------${this.email.from} wrote:\n> ${text}`
+    };
+  }
+
+  onSubmit(email: Email) {
+
+  }
 }
